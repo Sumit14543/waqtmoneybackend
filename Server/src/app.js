e15d979deb2ff8b9a87c283991697653f5d9e1fe
Sumit Kumar <sumitlodhi9401@gmail.com>
2026-06-17 23:23:55 +0530

    fix: allow lowercase headers in CORS and fallback auth in session middleware

--- Server/src/app.js
+++ Server/src/app.js
@@ -51,7 +51,7 @@ const applyCorsHeaders = (req, res) => {
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Repayment-Access-Token",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Repayment-Access-Token, X-Application-Id, X-Application-Mobile, X-Application-Email, X-Application-Pan, X-Application-Upload-Token, x-repayment-access-token, x-application-id, x-application-mobile, x-application-email, x-application-pan, x-application-upload-token",
   );
 
   return true;
@@ -78,6 +78,17 @@ const corsOptions = {
     "Accept",
     "Authorization",
     "X-Repayment-Access-Token",
+    "X-Application-Id",
+    "X-Application-Mobile",
+    "X-Application-Email",
+    "X-Application-Pan",
+    "X-Application-Upload-Token",
+    "x-repayment-access-token",
+    "x-application-id",
+    "x-application-mobile",
+    "x-application-email",
+    "x-application-pan",
+    "x-application-upload-token",
   ],
 };
 
